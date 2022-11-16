5af0efcc8f20c2d87bf698e2e837afc85c98579b
lmila29 <millcrylova@yandex.ru>
2022-11-16 23:58:57 +0300

    запись решения в файл readme.md

---
+++ HW2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (max > b) max = a;
+if (max > c) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+Console.Write("max=");
+Console.WriteLine(max);
+
