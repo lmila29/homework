71136678311acd790cb16b1140279dbc784b762b
lmila29 <millcrylova@yandex.ru>
2022-11-16 22:49:27 +0300

    первая задача

---
+++ HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if (a > b) max = a;
+if (a > b) min = b;
+if (b > a) max = b;
+if (b > a) min = a;
+
+Console.Write("max=");
+Console.WriteLine(max);
+Console.Write("min=");
+Console.WriteLine(min);
